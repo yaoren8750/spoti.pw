9e37bdfc1872f17258bd9225530af43c412c7020
yaoren8750 <7788mp3@gmail.com>
2026-09-25 17:08:21 +0800

    Sync local source

--- tweak/Sources/App/About/Licenses.m
+++ tweak/Sources/App/About/Licenses.m
@@ -31,8 +31,12 @@ static NSString *const kZlib =
 
 UIViewController *SGLicensesPage(void) {
     SGModRow *mod = SGLinkRow(@"spoti.pw", @"PolyForm Strict License 1.0.0", [SGRepoURL stringByAppendingString:@"/blob/main/LICENSE"]);
-    SGModRow *bs2b = SGLinkRow(@"交叉馈音 · MIT 许可证", @"https://github.com/alexmarsev/libbs2b");
-    SGModRow *wdl = SGLinkRow(@"WDL", @"Liveprog的 EEL2 · zlib 许可证", @"https://github.com/justinfrankel/WDL");
+    SGModRow *bs2b = SGLinkRow(@"libbs2b",
+                               @"交叉馈音 · MIT 许可证",
+                               @"https://github.com/alexmarsev/libbs2b");
+    SGModRow *wdl = SGLinkRow(@"WDL",
+                              @"Liveprog 的 EEL2 · zlib 许可证",
+                              @"https://github.com/justinfrankel/WDL");
     return [[SGModPage alloc] initWithTitle:@"许可证" intro:@"本 Mod 使用的许可证，以及所包含的第三方代码。" sections:@[
         SGSection(nil, @[mod]),
         SGNotedSection(nil, @[bs2b], [@"Copyright (c) 2005 Boris Mikhaylov\n\n" stringByAppendingString:kMIT]),
